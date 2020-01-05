b5b14c36b5d64345c3a7101a4d3ebde3b5a8a92c
dwane richards <dwanerich@gmail.com>
2020-01-05 18:36:30 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,32 @@
-# Add your code here
+require 'pry'
+class Dog
+  
+  @@all = []
+  @@names = []
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+    @@names << name
+    save
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.print_all
+    @@names.each.uniq do |name|
+     p name
+    end
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.clear_all
+    @@all.clear
+  end
+  
+end
